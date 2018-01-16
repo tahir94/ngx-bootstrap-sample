1a3f4c568c04281d5c7d383d9dbdbf1db3363c4d
tahir94 <tahirkhan.on@gmail.com>
2018-01-17 01:23:41 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { ModalComponent } from "./modal/modal.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  bsModalRef: BsModalRef;
   title = 'app';
+  singleModel: string = '1';
+
+  constructor(private modalService: BsModalService){
+
+  }
+
+  openModalWithComponent() {
+    const list = [
+      'Open a modal with component',
+      'Pass your data',
+      'Do something else',
+      '...'
+    ];
+    this.bsModalRef = this.modalService.show(ModalComponent);
+    this.bsModalRef.content.title = 'Modal with component';
+    this.bsModalRef.content.list = list;
+    setTimeout(() => {
+      list.push('PROFIT!!!');
+    }, 2000);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// ngx-bootstrap imports
+
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { AlertModule } from 'ngx-bootstrap';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent
+  ],
+  entryComponents:[
+    ModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModalModule.forRoot(),
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
